518d33bc5977b7527698e04d6f7c8172a816e497
xiaojiu zhe <kiki555888@sina.com>
2022-05-06 22:51:36 +0800

    1

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const sectionservice = document.querySelector(".service");
 const sectionteam = document.querySelector(".team");
 const sectionlocation = document.querySelector(".location");
 /*button flow */
-const buttonaction = document.querySelector(".btn-action");
+/*const buttonaction = document.querySelector(".btn-action");*/
 /*eyes flow */
 /*const eyesaction = document.querySelector(".eyesaction");*/
 /*bg change*/
@@ -36,20 +36,19 @@ nav[3].addEventListener("click",function(e){
     sectionservice.style.display="none";
     sectionteam.style.display="none";
     sectionlocation.style.display="grid";    
-})
+});
 /*button flow listener*/
-    buttonaction.addEventListener("click",function(e){
+/*  buttonaction.addEventListener("click",function(e){
     buttonaction.style.position="absolute";
     buttonaction.style.left="700px";
-    buttonaction.style.top="80px";
+    buttonaction.style.top="80px";*/
     /*eyesaction.src = "./images/homeye.png";*/
     /*setTimeout(() => {
         buttonaction.style.left="0px";
         buttonaction.style.top="0px";
         buttonaction.style.position="inherit";
     }, 3000);*/
-    
-});
+/*});*/
 /*backgroundcolor change listener*/
     mainbg.addEventListener("mousewheel",function(e){
         var r = Math.floor(Math.random() * 256);
