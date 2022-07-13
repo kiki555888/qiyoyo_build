ed236bb9620b4c2048d6dae09774cc1079cb8d36
xiaojiu zhe <kiki555888@sina.com>
2022-07-14 00:02:58 +0800

    1

--- js/customize.js
+++ js/customize.js
@@ -1,25 +1,35 @@
-// Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-    scrollFunction();
-};
-
-function scrollFunction() {
-    if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-    ) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+//===== Prealoder
+    window.onload = function () {
+        window.setTimeout(fadeout, 500);
     }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+    function fadeout() {
+        document.querySelector('.preloader').style.opacity = '0';
+        document.querySelector('.preloader').style.display = 'none';
+    }
+    
+ //   Sticky top menu
+window.onscroll = function () {
+        var header_navbar = document.querySelector("#navbar-area");
+        var sticky = header_navbar.offsetTop;
+
+        if (window.pageYOffset > sticky) {
+            header_navbar.classList.add("sticky");
+        } else {
+            header_navbar.classList.remove("sticky");
+        }
+
+// show or hide the back-top-top button
+        var backToTo = document.querySelector("#btn-back-to-top");
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            backToTo.style.display = "flex";
+        } else {
+            backToTo.style.display = "none";
+        }
+ // When the user clicks on the button, scroll to the top of the document
+     backToTo.addEventListener('click', backToTop);
+
+       function backToTop() {
+             document.body.scrollTop = 0;
+             document.documentElement.scrollTop = 0;}
+    };
