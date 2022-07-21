fb2582cde68d979874b73fb96ae081ef7fb41d98
xiaojiu zhe <kiki555888@sina.com>
2022-07-22 00:07:35 +0800

    1

--- js/customize.js
+++ js/customize.js
@@ -34,10 +34,3 @@ window.onscroll = function () {
         document.documentElement.scrollTop = 0;
     }
 };
-var LHC_API = LHC_API || {};
-LHC_API.args = { mode: 'widget', lhc_base_url: '//yundingxm.com:8080/index.php/', wheight: 450, wwidth: 350, pheight: 520, pwidth: 500, leaveamessage: true, theme: "1", check_messages: false, lang: 'chn/' };
-(function () {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin', 'anonymous'); po.async = true;
-    var date = new Date(); po.src = '//yundingxm.com:8080/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-})();
