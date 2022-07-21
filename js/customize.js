ef8b79a0df8c2836f97272825b003ab9b96a06cc
xiaojiu zhe <kiki555888@sina.com>
2022-07-21 23:46:40 +0800

    1

--- js/customize.js
+++ js/customize.js
@@ -1,35 +1,43 @@
 //===== Prealoder
-    window.onload = function () {
-        window.setTimeout(fadeout, 500);
-    }
+window.onload = function () {
+    window.setTimeout(fadeout, 500);
+}
 
-    function fadeout() {
-        document.querySelector('.preloader').style.opacity = '0';
-        document.querySelector('.preloader').style.display = 'none';
-    }
-    
- //   Sticky top menu
+function fadeout() {
+    document.querySelector('.preloader').style.opacity = '0';
+    document.querySelector('.preloader').style.display = 'none';
+}
+
+//   Sticky top menu
 window.onscroll = function () {
-        var header_navbar = document.querySelector("#navbar-area");
-        var sticky = header_navbar.offsetTop;
+    var header_navbar = document.querySelector("#navbar-area");
+    var sticky = header_navbar.offsetTop;
 
-        if (window.pageYOffset > sticky) {
-            header_navbar.classList.add("sticky");
-        } else {
-            header_navbar.classList.remove("sticky");
-        }
+    if (window.pageYOffset > sticky) {
+        header_navbar.classList.add("sticky");
+    } else {
+        header_navbar.classList.remove("sticky");
+    }
 
-// show or hide the back-top-top button
-        var backToTo = document.querySelector("#btn-back-to-top");
-        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            backToTo.style.display = "flex";
-        } else {
-            backToTo.style.display = "none";
-        }
- // When the user clicks on the button, scroll to the top of the document
-     backToTo.addEventListener('click', backToTop);
+    // show or hide the back-top-top button
+    var backToTo = document.querySelector("#btn-back-to-top");
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        backToTo.style.display = "flex";
+    } else {
+        backToTo.style.display = "none";
+    }
+    // When the user clicks on the button, scroll to the top of the document
+    backToTo.addEventListener('click', backToTop);
 
-       function backToTop() {
-             document.body.scrollTop = 0;
-             document.documentElement.scrollTop = 0;}
-    };
+    function backToTop() {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    }
+};
+var LHC_API = LHC_API || {};
+LHC_API.args = { mode: 'widget', lhc_base_url: '//yundingxm.com:8080/index.php/', wheight: 450, wwidth: 350, pheight: 520, pwidth: 500, leaveamessage: true, theme: "1", check_messages: false, lang: 'chn/' };
+(function () {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.setAttribute('crossorigin', 'anonymous'); po.async = true;
+    var date = new Date(); po.src = '//yundingxm.com:8080/design/defaulttheme/js/widgetv2/index.js?' + ("" + date.getFullYear() + date.getMonth() + date.getDate());
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+})();
